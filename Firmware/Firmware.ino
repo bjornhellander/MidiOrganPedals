ff74903ee5b54bacc75e3566aea7438eb249cd00
Björn Hellander <bjorn_hellander@yahoo.se>
2017-08-06 00:24:31 +0200

    Added current firmware code (hello world style).

---
+++ Firmware/Firmware.ino
@@ -0,0 +1,32 @@
+/* Simple "Hello World" example.
+
+   After uploading this to your board, use Serial Monitor
+   to view the message.  When Serial is selected from the
+   Tools > USB Type menu, the correct serial port must be
+   selected from the Tools > Serial Port AFTER Teensy is
+   running this code.  Teensy only becomes a serial device
+   while this code is running!  For non-Serial types,
+   the Serial port is emulated, so no port needs to be
+   selected.
+*/
+
+const int ledPin = 6; // Teensy++ 2.0 has the LED on pin 6
+int count = 0;
+int ledOn = 1;
+
+void setup() {
+  Serial.begin(9600); // USB is always 12 Mbit/sec
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  count++;
+  Serial.print("Hello World (");
+  Serial.print(count);
+  Serial.println(")");
+  
+  ledOn = !ledOn;
+  digitalWrite(ledPin, ledOn);
+  
+  delay(1000);
+}
---
+++ Firmware/upload.cmd
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Arduino\arduino_debug.exe" --board teensy:avr:teensypp2 --upload --verbose Firmware.ino
